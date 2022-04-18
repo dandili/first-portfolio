30c7603fcb4674f0985a82b2ef7a10cf4840b4f8
dandili <daniele.d@hotmail.co.uk>
2022-04-18 18:51:27 +0100

    Updated CSS font, colours and other UI aspects.

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-    $('.menu-toggler').on('click', function () {
+    $('.menu-toggle').on('click', function () {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
     });
 
     $('.top-nav .nav-link').on('click', function () {
-        $('.menu-toggler').removeClass('open');
+        $('.menu-toggle').removeClass('open');
         $('.top-nav').removeClass('open');
     });
 
